e53ecfed512c39aeb35a33f60a29e76d118438de
Reiro <reiro123@gmail.com>
2016-05-19 14:11:55 +0300

    add validate token to facebook init

--- config/initializers/messenger_bot.rb
+++ config/initializers/messenger_bot.rb
@@ -1,5 +1,5 @@
-# Messenger::Bot.config do |config|
-#   config.access_token = <ACCESS_TOKEN>
-#   config.validation_token = <VERIFY_TOKEN>
-#   config.secret_token = <SECRET_TOKEN>
-# end
+Messenger::Bot.config do |config|
+  # config.access_token = 123123123123
+  config.validation_token = '123123123123'
+  config.secret_token = 'bfe714074d9869064b75f059f09011ef'
+end
