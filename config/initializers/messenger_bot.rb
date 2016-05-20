c0391be0542394d5cda060ddaa80fc63f85f4d10
Reiro <reiro123@gmail.com>
2016-05-20 18:38:29 +0300

    try 9

--- config/initializers/messenger_bot.rb
+++ config/initializers/messenger_bot.rb
@@ -5,7 +5,7 @@ Messenger::Bot.config do |config|
 end
 
 Facebook::Messenger.configure do |config|
-  config.access_token = 'EAAZAi54p5yFUBACiZAmA2Rle4bxU6Czk9q6NVAa4sZBeWcnMZBaZAsgHjCDW0VhuZCNgoFl5KtSpBpJpDyF98zaLlzEntYBpS2B6oZC0D6ZC5KmgNRLdYrlA000RScLu84ga6QBZCZCL7hZBtHKflZA8tKta9ZBKEb9rZB5hl7TwTJjFT4PwZDZD'
+  config.access_token = 'EAAZAi54p5yFUBABrXZCP4QvUiStJxWUy3CRcUfsLqidZBHTpXsUFJdgiKjGamFWjvFUsMPIaJGt433JzRU25uu5yJe4x1fhEZANI8D5Og6JKLyavqVEtmTKU8cgAKcNgM8ZBLrvG5SprDmAlQjFB5CvkJWFgLjZByE7VcYNjILZBgZDZD'
   #config.app_secret = 'bfe714074d9869064b75f059f09011ef'
   config.verify_token = '123123123123'
 end
